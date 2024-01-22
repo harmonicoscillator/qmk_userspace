e65944f58ec1920a5f4a4b896beb6e06c146b42f
R. Alex Barbieri <>
2024-01-21 18:53:58 -0600

    working build of crkbd

---
+++ keyboards/crkbd/rev1/keymaps/alex-miryoku/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define EE_HANDS
+
+#define USE_SERIAL_PD2
+#undef USE_I2C
+
+#define OLED_BRIGHTNESS 50
+#define OLED_UPDATE_INTERVAL 50
---
+++ keyboards/crkbd/rev1/keymaps/alex-miryoku/rules.mk
@@ -0,0 +1,4 @@
+USER_NAME := harmonicoscillator
+BOOTLOADER = atmel-dfu
+OLED_ENABLE = yes
+RGBLIGHT_ENABLE = no
